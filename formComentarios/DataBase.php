5196d4e82cf4b74f6d8ce10ce3922960cad6b147
Mef-Cat <luisuriel.rojo1@gmail.com>
2020-06-09 16:58:46 -0500

    Formulario 2.0v

---
+++ formComentarios/DataBase.php
@@ -0,0 +1,9 @@
+<?php
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$like = $_POST['like'];
+$like2 = $_POST['like2'];
+$suge = $_POST['suge'];
+$terminos = $_POST['terminos'];
+echo "<h2>Hola </H2>".$nombre;
+ ?>
